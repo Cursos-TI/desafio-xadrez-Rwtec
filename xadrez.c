a94474260508a41221cfd556c67c35ff43ac7e08
Wesley Melo <138056546+Rwtec@users.noreply.github.com>
2025-11-11 03:34:16 +0000

    Neste desafio,continuidade ao programa do nível básico, adicionando a lógica para movimentar o Cavalo no tabuleiro de xadrez. A principal diferença é que o Cavalo se move em "L", o que exigirá o uso de loops aninhados (um loop dentro do outro) para simular esse movimento.
     no mesmo programa em C do desafio anterior, a lógica para o movimento do Cavalo. O Cavalo se move duas casas em uma direção (horizontal ou vertical) e depois uma casa perpendicularmente, formando um "L". Para este desafio, o Cavalo deverá se mover duas casas para baixo e uma casa para a esquerda. Você precisará usar pelo menos dois loops aninhados, sendo um deles obrigatoriamente um loop for. O outro loop pode ser while ou do-while, à sua escolha. Assim como nas outras peças, você imprimirá a direção do movimento a cada casa percorrida.
    
    Requisitos funcionais
    
    Entrada de Dados: Os valores para o número de casas a serem movidas (duas para baixo, uma para a esquerda) definidos no código como variáveis ou constantes.
    
    Lógica de Movimentação: O programa implementar a lógica específica do movimento em "L" do Cavalo usando loops aninhados (um loop for e um loop while ou do-while).
    
    Saída de Dados: O programa imprimir no console a direção de cada etapa do movimento do Cavalo ("Baixo", "Baixo", "Esquerda"), utilizando o comando printf. Para as outras peças as saídas devem seguir o padrão: printf("Cima\n");, printf("Baixo\n");, printf("Esquerda\n");, printf("Direita\n");. Separe o movimento do Cavalo dos movimentos anteriores com uma linha em branco.
    
    Requisitos não funcionais
    
    Não é necessário validar a entrada do usuário.
    
    Não é necessário implementar outras regras do xadrez além do movimento específico do Cavalo solicitado.
    
    Você pode assumir que o Cavalo sempre começa na posição inicial (definida por você).

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-   int main(){
+   int main() {
 
    int movimentoTorre = 0; //A Torre move-se em linha reta horizontalmente ou verticalmente.
    int movimentobispo = 0; //Move-se na diagonal. Seu programa deverá simular o movimento do Bispo cinco casas na diagonal para cima e à direita. Para representar a diagonal, você imprimirá a combinação de duas direções a cada casa (ex: "Cima, Direita").
    int movimentoRainha = 0; // Move-se em todas as direções. Seu programa deverá simular o movimento da Rainha oito casas para a esquerda.
-   
+   int movimentoCavalo = 0; // O cavalo se movimenta em "L" ex("baixo,baixo,esquerda")
 
    // movimento da torre usado o for 
    printf("******Jogo de xadrez !*******\n");
@@ -17,8 +17,8 @@
    }
 
    // movimento do bispo usado o while
-   printf("\nMovimento do Bispo.\n");
-   printf("O Bispo move-se na diagonal.\n");
+   printf("\nMovimento do Bispo!\n");
+   printf("O Bispo move-se na diagonal\n");
    printf("Movendo 5 casas na diagonal para cima e direita:\n");
    
    while ( movimentobispo < 5 )
@@ -27,7 +27,7 @@
       
    }
    //movimento da Rainha usado do while
-   printf("\n Movimento da Rainha \n");
+   printf("\n Movimento da Rainha !\n");
    printf("A Rainha move-se em todas as direções. \n");
    printf("Movendo 8 casas para a esquerda: \n");
    do {
@@ -36,8 +36,22 @@
    } 
       while (movimentoRainha < 8);
    
-      printf ("\n Fim dos movimentos! \n");
-    
+   // A principal diferença é que o Cavalo se move em "L", o que exigirá o uso de loops aninhados (um loop dentro do outro) para simular esse movimento
+    printf("\nMovimento do Cavalo !\n");
+    printf("O cavalo move-se em 'L'.");
+    printf("Movendo-se duas casa na vertical/horizotal e uma casa na vertical/horizontal!\n");
+   for (movimentoCavalo = 0; movimentoCavalo < 2; ++movimentoCavalo)
+    { int cavalo = 0;
+      while (cavalo < 1)
+      {
+         printf("baixo\n");
+         cavalo++;
+      }
+      
+      }
+      printf("Esquerda\n");
+     
+   printf ("\n Fim dos movimentos! \n");
       return 0;
    
    }
